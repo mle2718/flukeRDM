f5a3106e39cc8e60c9e42fc67dc9b567860db610
Kimberly.Bastille <1567716968113305@mil>
2025-08-17 10:14:02 -0400

    update model runs DE, MD, NJ

--- recDST/model_run_NJ.R
+++ recDST/model_run_NJ.R
@@ -5,16 +5,17 @@ Run_Name <- args[1]
 
 saved_regs<- read.csv(here::here(paste0("saved_regs/regs_", Run_Name, ".csv")))
 
-for (a in seq_len(nrow(save_regs))) {
+for (a in seq_len(nrow(saved_regs))) {
   # Extract name and value
-  obj_name <- save_regs$input[a]
-  obj_value <- save_regs$value[a]
+  obj_name <- saved_regs$input[a]
+  obj_value <- saved_regs$value[a]
   
   # Assign to object in the environment
   assign(obj_name, obj_value)
 }
 
-
+start_time<- Sys.time()
+# directed_trips<- directed_trips %>%  
 print("start model_NJ")
 state1 = "NJ"
 predictions_all = list()
@@ -23,21 +24,21 @@ data_path <- here::here("Data/")
 
 
 #### Read in size data ####
-sf_size_data <- readr::read_csv(file.path(data_path, "fluke_projected_catch_at_lengths.csv"), show_col_types = FALSE)  %>% 
-  dplyr::filter(state == "NJ") %>% 
+size_data <- readr::read_csv(file.path(here::here("Data"), "baseline_catch_at_length.csv"), show_col_types = FALSE)  %>% 
+  dplyr::filter(state == "NJ")
+
+sf_size_data <- size_data %>% 
+  dplyr::filter(species=="sf") %>% 
   dplyr::filter(!is.na(fitted_prob)) %>% 
   dplyr::select(state, fitted_prob, length, draw)
-
-bsb_size_data <- readr::read_csv(file.path(data_path, "bsb_projected_catch_at_lengths.csv"), show_col_types = FALSE)  %>% 
-  dplyr::filter(state == "NJ") %>% 
+bsb_size_data <- size_data  %>% 
+  dplyr::filter(species=="bsb") %>% 
   dplyr::filter(!is.na(fitted_prob)) %>% 
   dplyr::select(state, fitted_prob, length, draw)
-
-scup_size_data <- readr::read_csv(file.path(data_path, "scup_projected_catch_at_lengths.csv"), show_col_types = FALSE)  %>% 
-  dplyr::filter(state == "NJ") %>% 
+scup_size_data <- size_data %>% 
+  dplyr::filter(species=="scup") %>% 
   dplyr::filter(!is.na(fitted_prob)) %>% 
   dplyr::select(state,  fitted_prob, length, draw)
-
 
 l_w_conversion <- readr::read_csv(file.path(data_path, "L_W_Conversion.csv"), show_col_types = FALSE)  %>% 
   dplyr::filter(state=="NJ")
@@ -45,8 +46,8 @@ l_w_conversion <- readr::read_csv(file.path(data_path, "L_W_Conversion.csv"), sh
 #### directed trips ####
 directed_trips<-feather::read_feather(file.path(data_path, paste0("directed_trips_calibration_NJ.feather"))) %>% 
   tibble::tibble() %>%
-  dplyr::select(mode, date, draw, bsb_bag_y2, bsb_min_y2, fluke_bag,fluke_min, scup_bag_y2, scup_min_y2,
-                bsb_bag_y2_y2, bsb_min_y2_y2, fluke_bag_y2,fluke_min_y2, scup_bag_y2_y2, scup_min_y2_y2) %>% 
+  dplyr::select(mode, date, draw, bsb_bag, bsb_min, fluke_bag,fluke_min, scup_bag, scup_min,
+                bsb_bag_y2, bsb_min_y2, fluke_bag_y2,fluke_min_y2, scup_bag_y2, scup_min_y2) %>% 
   dplyr::mutate(date_adj = lubridate::dmy(date), 
                 date_adj = lubridate::yday(date_adj)) 
 
@@ -162,25 +163,31 @@ directed_trips<- directed_trips %>%
     scup_min_y2=dplyr::case_when(mode == "pr" & day_i >= lubridate::yday(SCUPnjPR_seas2_op) & day_i <= lubridate::yday(SCUPnjPR_seas2_cl) ~ as.numeric(SCUPnjPR_2_len) * 2.54, TRUE ~ scup_min_y2), 
     scup_min_y2=dplyr::case_when(mode == "sh" & day_i >= lubridate::yday(SCUPnjSH_seas2_op) & day_i <= lubridate::yday(SCUPnjSH_seas2_cl) ~ as.numeric(SCUPnjSH_2_len) * 2.54, TRUE ~ scup_min_y2))
     
+
 predictions_out10 <- data.frame()
 #future::plan(future::multisession, workers = 36)
 #future::plan(future::multisession, workers = 3)
 #get_predictions_out<- function(x){
-for(x in 1:1){
+for(x in 1:3){
   
   print(x)
   
-  directed_trips <- directed_trips %>% 
+  directed_trips2 <- directed_trips %>% 
     dplyr::filter(draw == x) # %>%
   # dplyr::mutate(day = stringr::str_extract(day, "^\\d{2}"), 
   #               period2 = paste0(month24, "-", day, "-", mode))
   
   catch_data <- feather::read_feather(file.path(data_path, paste0("projected_catch_draws_NJ", "_", x,".feather"))) %>% 
-    dplyr::left_join(directed_trips, by=c("mode", "date", "draw")) 
+    dplyr::left_join(directed_trips2, by=c("mode", "date", "draw")) 
   
   catch_data<-catch_data %>% 
     dplyr::select(-cost, -total_trips_12, -age, -bsb_keep_sim, -bsb_rel_sim, -day_i, -my_dom_id_string, 
                   -scup_keep_sim, -scup_rel_sim, -sf_keep_sim, -sf_rel_sim, -wave)
+  
+  calendar_adjustments <- readr::read_csv(
+    file.path(here::here(paste0("Data/proj_year_calendar_adjustments_NJ.csv"))), show_col_types = FALSE) %>%
+    dplyr::filter(state == "NJ", draw==x) %>% 
+    dplyr::select(-dtrip, -dtrip_y2, -state, -draw)
   
   
   base_outcomes0 <- list()
@@ -216,31 +223,80 @@ for(x in 1:1){
   
   
   # Pull in calibration comparison information about trip-level harvest/discard re-allocations 
-  calib_comparison<-feather::read_feather(file.path(data_path, "calibration_comparison.feather")) %>% 
-    dplyr::filter(state=="NJ" & draw==x )   
+  calib_comparison<-readRDS(file.path(data_path,"calibrated_model_stats.rds")) %>%
+    dplyr::filter(state=="NJ" & draw==x )  
   
-  sf_size_data <- sf_size_data %>% 
+  calib_comparison<-calib_comparison %>% 
+    dplyr::rename(n_legal_rel_bsb=n_legal_bsb_rel, 
+                  n_legal_rel_scup=n_legal_scup_rel, 
+                  n_legal_rel_sf=n_legal_sf_rel, 
+                  n_sub_kept_bsb=n_sub_bsb_kept,
+                  n_sub_kept_sf=n_sub_sf_kept,
+                  n_sub_kept_scup=n_sub_scup_kept,
+                  prop_legal_rel_bsb=prop_legal_bsb_rel,
+                  prop_legal_rel_sf=prop_legal_sf_rel,
+                  prop_legal_rel_scup=prop_legal_scup_rel,
+                  prop_sub_kept_bsb=prop_sub_bsb_kept,
+                  prop_sub_kept_sf=prop_sub_sf_kept,
+                  prop_sub_kept_scup=prop_sub_scup_kept,
+                  convergence_sf=sf_convergence,
+                  convergence_bsb=bsb_convergence,
+                  convergence_scup=scup_convergence) 
+  
+  ##########
+  # List of species suffixes
+  species_suffixes <- c("sf", "bsb", "scup")
+  
+  # Get all variable names
+  all_vars <- names(calib_comparison)
+  
+  # Identify columns that are species-specific (contain _sf, _bsb, or _scup)
+  species_specific_vars <- all_vars[
+    stringr::str_detect(all_vars, paste0("(_", species_suffixes, ")$", collapse = "|"))
+  ]
+  
+  id_vars <- setdiff(all_vars, species_specific_vars)
+  
+  calib_comparison<-calib_comparison %>% 
+    dplyr::select(mode, all_of(species_specific_vars))
+  
+  # Extract base variable names (without _sf, _bsb, _scup)
+  base_names <- unique(stringr::str_replace(species_specific_vars, "_(sf|bsb|scup)$", ""))
+  
+  # Pivot the data longer on the species-specific columns
+  calib_comparison <- calib_comparison %>%
+    tidyr::pivot_longer(
+      cols = all_of(species_specific_vars),
+      names_to = c(".value", "species"),
+      names_pattern = "(.*)_(sf|bsb|scup)"
+    ) %>% 
+    dplyr::distinct()
+  
+  sf_size_data2 <- sf_size_data %>% 
     dplyr::filter(draw == x) %>%  #Change to X for model for sf and scup
     dplyr::select(-draw)
   
   ### Change when bsb_size is updated
-  bsb_size_data <- bsb_size_data %>% 
-    dplyr::filter(draw == 0) %>% 
+  bsb_size_data2 <- bsb_size_data %>% 
+    dplyr::filter(draw == x) %>% 
     dplyr::select(-draw)
   
-  scup_size_data <- scup_size_data %>% 
+  scup_size_data2 <- scup_size_data %>% 
     dplyr::filter(draw == x) %>% 
     dplyr::select(-draw)
   
   
   ## Run the predict catch function
+  source(here::here("Code/sim/predict_rec_catch_functions.R"))
   source(here::here("Code/sim/predict_rec_catch.R"))
   
   test<- predict_rec_catch(st = "NJ", dr = x,
-                           directed_trips, catch_data, 
-                           sf_size_data, bsb_size_data, scup_size_data, 
+                           directed_trips = directed_trips2, catch_data, 
+                           sf_size_data = sf_size_data2,
+                           bsb_size_data = bsb_size_data2, 
+                           scup_size_data = scup_size_data2, 
                            l_w_conversion, calib_comparison, n_choice_occasions, 
-                           base_outcomes)
+                           calendar_adjustments, base_outcomes)
   
   test <- test %>% 
     dplyr::mutate(draw = c(x),
@@ -263,7 +319,7 @@ print("out of loop")
 #predictions_out10<- furrr::future_map_dfr(1:3, ~get_predictions_out(.), .id = "draw")
 
 #readr::write_csv(predictions_out10, file = here::here(paste0("output/output_MA_", Run_Name, "_", format(Sys.time(), "%Y%m%d_%H%M%S"),  ".csv")))
-readr::write_csv(predictions_out10, file = here::here(paste0("output/output_NJ_Alt_", format(Sys.time(), "%Y%m%d_%H%M%S"),  ".csv")))
+readr::write_csv(predictions_out10, file = here::here(paste0("output/output_NJ_", Run_Name, "_", format(Sys.time(), "%Y%m%d_%H%M%S"),  ".csv")))
 
 
 end_time <- Sys.time()
