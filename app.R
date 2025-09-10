370aefa1744e47d3879236e4ea390793b95494c5
Min-Yang Lee <Min-Yang.Lee@noaa.gov>
2025-09-10 11:55:38 -0400

    revise first line of text. move up run name box.

--- app.R
+++ app.R
@@ -93,7 +93,10 @@ ui <- fluidPage(
              
     
     tabPanel( "Regulation Selection",
-              strong(div("REMINDER: (1) select state(s)  (2) Make selections below (3) click run me and then the `Results` tab to run model", style = "color:blue")), # Warning for users
+              strong(div("INSTRUCTIONS: (1) Give your run a name, (2) select one or more states,  (3) Select regulations, (4) click run me and wait for the model to run, (5) Use the `Results` tab to examine the results", style = "color:blue")), # Warning for users
+              # Collect the Run Name
+              textInput("Run_Name", "Please name this using your initials and the number of the run (ex. AB1)."),
+              
               shinyWidgets::awesomeCheckboxGroup( # Select which state(s) to run
                 inputId = "state", 
                 label = "State", 
@@ -103,7 +106,7 @@ ui <- fluidPage(
               
               #Run Button
               actionButton("runmeplease", "Run Me"), 
-              textInput("Run_Name", "Please name this using your initials and the number of the run (ex. AB1)."),
+              
               textOutput("message"),
               # Add UI code for each state
               uiOutput("addMA"),
