3d59a8c6c151c873490d1be205fa6837e7a22634
Kimberly.Bastille <1567716968113305@mil>
2025-11-19 13:15:10 -0500

    remove 54

--- recDST/model_run_VA.R
+++ recDST/model_run_VA.R
@@ -302,7 +302,7 @@ print("out of loop")
 
 # use furrr package to parallelize the get_predictions_out function 100 times
 # This will spit out a dataframe with 100 predictions 
-predictions_out10<- furrr::future_map_dfr(c(1:52, 54:100), ~get_predictions_out(.), .id = "draw")
+predictions_out10<- furrr::future_map_dfr(c(1:52, 55:100), ~get_predictions_out(.), .id = "draw")
 #predictions_out10<- furrr::future_map_dfr(1:25, ~get_predictions_out(.), .id = "draw")
 
 #readr::write_csv(predictions_out10, file = here::here(paste0("output/output_MA_", Run_Name, "_", format(Sys.time(), "%Y%m%d_%H%M%S"),  ".csv")))
