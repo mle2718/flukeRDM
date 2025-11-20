01ce378cbe1b65e5f6dd66f2cdc8fb021cfda5fe
Kimberly.Bastille <1567716968113305@mil>
2025-11-20 13:16:18 -0500

    cut down CT and add indicator

--- recDST/model_run_CT.R
+++ recDST/model_run_CT.R
@@ -1,6 +1,10 @@
 ##############################
 ### CT Rec model run  ########
 ##############################
+
+# INDICATOR
+
+
 Run_Name <- args[1]
 
 saved_regs<- read.csv(here::here(paste0("saved_regs/regs_", Run_Name, ".csv")))
@@ -131,7 +135,7 @@ directed_trips<- directed_trips %>%
 
 predictions_out10 <- data.frame()
 #future::plan(future::multisession, workers = 36)
-future::plan(future::multisession, workers = 100)
+future::plan(future::multisession, workers = 5)
 get_predictions_out<- function(x){
 #for(x in 1:25){
   
@@ -274,11 +278,11 @@ get_predictions_out<- function(x){
 
 print("out of loop")
 
-
+start_time <- Sys.time()
 
 # use furrr package to parallelize the get_predictions_out function 100 times
 # This will spit out a dataframe with 100 predictions 
-predictions_out10<- furrr::future_map_dfr(1:100, ~get_predictions_out(.), .id = "draw")
+predictions_out10<- furrr::future_map_dfr(1:5, ~get_predictions_out(.), .id = "draw")
 #predictions_out10<- furrr::future_map_dfr(1:25, ~get_predictions_out(.), .id = "draw")
 
 #readr::write_csv(predictions_out10, file = here::here(paste0("output/output_MA_", Run_Name, "_", format(Sys.time(), "%Y%m%d_%H%M%S"),  ".csv")))
